ba222f5ccfa26f8d93542420c1e268d52f273c4e
Pedro Scheidt <pedrohenriqueprazeres@hotmail.com>
2024-02-21 16:04:37 -0300

    clean up of debug code + funtionalized

--- c4/c4.go
+++ c4/c4.go
@@ -11,7 +11,6 @@ package c4
 
 import (
 	"Crypto_Pals_Questions/c3"
-	"fmt"
 	"os"
 	"strings"
 )
@@ -24,19 +23,13 @@ func divideStrings(sorcePath string) []string{
 		panic(err)
 	}
 	stringsList := strings.Split(string(sampleFile), "\n")
-	/* for index, v := range stringsList{
-		fmt.Printf("%v: %v\n", index, v)
-	} */
 	return stringsList
 }
 
 func turnIntoProbables(stringList []string, realTextFrequency map[rune]float64) []string{
-	//fmt.Printf("%v", realTextFrequency)
 	bestVersions := make([]string, len(stringList))
 	for _, v := range stringList{
-		//fmt.Printf("%v \n", v)
 		meaning, _, err :=  c3.DecodeSingleXOR(v, realTextFrequency)
-		fmt.Println(meaning)
 		if err != nil{
 			panic(err)
 		}
@@ -44,27 +37,19 @@ func turnIntoProbables(stringList []string, realTextFrequency map[rune]float64) 
 	}
 	return bestVersions
 }
-	/* var probableTextIndex int
-	var probableTextKey rune
-	var probableTextGrade float64
-	for index, sample := range stringsList{
-		meaning, key, err :=
-		if err != nil{
-			err := fmt.Errorf("%v: error in line 39", err)
-			panic(err)
-		}
-		grade := c3.EnglishScore(meaning, realTextFrequency)
-		if grade > probableTextGrade {
-			probableTextGrade = grade
-			probableTextIndex = index
-			probableTextKey = key
+
+func findMessage(textPath string, realTextPath string) string{
+	realTextFrequency := c3.CharFrequencyMap("", realTextPath)
+	strings := divideStrings(textPath)
+	strings = turnIntoProbables(strings, realTextFrequency)
+	topGrade := float64(0)
+	topIndex := 0
+	for index, string := range strings{
+		grade := c3.EnglishScore(string, realTextFrequency)
+		if grade > topGrade{
+			topGrade = grade
+			topIndex = index
 		}
 	}
-	bytedText, err := hex.DecodeString(stringsList[probableTextIndex])
-	if err != nil{
-		panic(err)
-	}
-	decodedText := c3.SingleXOR(bytedText, byte(probableTextKey))
-	decryptedMessage := hex.EncodeToString(decodedText)
-	fmt.Printf("The line %v probably means %v, with the key %c", probableTextIndex+1, decryptedMessage,probableTextKey)
-} */
+	return strings[topIndex]
+}
